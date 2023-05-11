a4517c5fb4fc33fa63abd27e186f6345b43d3c0c
Serhedo <serhat.alpayci@gmx.de>
2023-05-11 10:16:32 +0200

    2a. Parametrisierung + Datenübergabe von INNEN

--- script_01.js
+++ script_01.js
@@ -3,3 +3,26 @@
 /***** Funktionen 01 *****/
 // 1. Kapselung von Codeblöcken
 
+// Funktionsaufruf (call)
+// test();
+
+// Funktionsrumpf (body) | callee
+// Funktionsdeklaration
+function test()
+{
+    console.log("Hallo Sero Mero!");
+}
+
+/***** Funktionen 02a *****/
+// 2a. Parametrisierung + Datenübergabe von INNEN
+
+ausgabeNamen();
+function ausgabeNamen() 
+{
+    // what happens in VEGAS ...
+    let firstName = "Karsten";
+    console.log("Hallo " + firstName + "!");
+
+}
+
+// console.log(firstName); // Fehler: Scope!
